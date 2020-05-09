d9bcead9326130967b57fa3eccaedd5af3c6f286
Christophe <Christophe@LAPTOP-8U4ADOMS>
2020-05-09 16:11:59 +0200

    add shadows image

--- magicFlash.py
+++ magicFlash.py
@@ -2,6 +2,7 @@ from skimage import io
 import tifffile as tif
 import numpy as np
 import scipy
+import matplotlib
 
 def moy(img):
     return (img[:,:,0] + img [:,:,1] + img[:,:,2])/3
@@ -39,14 +40,50 @@ def uint16_gamma(img):
 def filter_diff(res, b, colour, sigma):
     res2 = res[:,:,colour]
     b2 = b[:,:,colour]
-    b0 = b2 != 0
-    b_diff = b2.copy()
-    b_diff[b0] = res2[b0] /  b2[b0]
 
-    b_diff2 = scipy.ndimage.gaussian_filter(b_diff,sigma)
-    b_diff2 =  b_diff2 * b2
-    b_diff2[np.logical_not(b0)] = res2[np.logical_not(b0)]
-    return b_diff2
+    diff1 = scipy.ndimage.gaussian_filter(res2, sigma)
+    diff2 = scipy.ndimage.gaussian_filter(b2, sigma)
+    b0 = diff2 >  0
+    filt_image = b2.copy()
+    filt_image[b0] = diff1[b0]/diff2[b0]
+    filt_image = filt_image * b2
+    filt_image[np.logical_not(b0)] = res2[np.logical_not(b0)]
+    return filt_image
+
+def compute_shadows(ambiant, flash):
+    hsv1 = matplotlib.colors.hsv_to_rgb(ambiant)[:,:,2]
+    hsv2 = matplotlib.colors.hsv_to_rgb(flash)[:,:,2]
+    hsv1 = scipy.ndimage.gaussian_filter(hsv1, 6)
+    hsv2 = scipy.ndimage.gaussian_filter(hsv2, 6)
+
+    v0 = hsv2 != 0
+    
+    res = hsv1.copy()
+    res[v0] = hsv1[v0]/hsv2[v0]
+    m = np.average(res[v0])
+    res[v0] = res[v0]/m
+    res[np.logical_not(v0)] = 1
+    res[res < 0] = 1
+
+    res2 = 0.2 * np.log(res)
+
+    res3 = ambiant.copy()
+    pos2 = res2 > 0
+    neg2 = res2 <= 0
+
+    zeros = np.zeros(
+        (res2.shape[0], res2.shape[1],)
+    )
+
+    res3[neg2, 0] = -res2[neg2]
+    res3[neg2, 1] = zeros[neg2]
+    res3[neg2, 2] = zeros[neg2]
+
+    res3[pos2, 0] = zeros[pos2]
+    res3[pos2, 1] = res2[pos2]
+    res3[pos2, 2] = zeros[pos2]
+
+    return res3 
 
 a = tif.imread("D:/images/fillfllash/Capture/xxx0000.tif")
 b = tif.imread("D:/images/fillfllash/Capture/xxx0001.tif")
@@ -62,21 +99,23 @@ s = a.shape
 maxv = 65535
 c = 16.0/16.0
     
-a = a[400:2800, 500:4500,:]
-b = b[400:2800, 500:4500,:]
+#a = a[400:2800, 500:4500,:]
+#b = b[400:2800, 500:4500,:]
 
 a = a/maxv
 b = b/maxv
 
 res = 20*(b - a)
-        
-filter_r = filter_diff(res, b, 0, 6)
-filter_g = filter_diff(res, b, 1, 6) 
-filter_b = filter_diff(res, b, 2, 6)
+sup0(res)
+
+filter_r = filter_diff(res, b, 0, 5.8)
+filter_g = filter_diff(res, b, 1, 5.8) 
+filter_b = filter_diff(res, b, 2, 5.8)
 
 img3 = np.stack(
     [filter_r, filter_g, filter_b], axis=2
 )
-
+shadows = compute_shadows(a, b)
 tif.imsave('D:/images/fillfllash/Capture/only_flash.tif', uint16_gamma(res))
 tif.imsave('D:/images/fillfllash/Capture/clean_result.tif', uint16_gamma(img3))
+tif.imsave('D:/images/fillfllash/Capture/shadows.tif', uint16_gamma(shadows))
